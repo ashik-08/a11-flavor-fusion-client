076084e58263b9f993209d75066556e7a11f8f04
ashik-08 <ashikurrahman.8october@gmail.com>
2023-11-06 01:30:13 +0600

    added react helmet package to change title for different routes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "../../components/Home/Banner/Banner";
 import ChefSpecialties from "../../components/Home/Chef_Special/ChefSpecialties";
 import CustomerReview from "../../components/Home/Customer_Review/CustomerReview";
@@ -5,6 +6,9 @@ import CustomerReview from "../../components/Home/Customer_Review/CustomerReview
 const HomePage = () => {
   return (
     <section>
+      <Helmet>
+        <title>Flavor Fusion | Home</title>
+      </Helmet>
       <Banner></Banner>
       <ChefSpecialties></ChefSpecialties>
       <CustomerReview></CustomerReview>
