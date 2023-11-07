44f207684590c5a9f2e4413df78409f46016c7b5
ashik-08 <ashikurrahman.8october@gmail.com>
2023-11-07 21:52:18 +0600

    added PrivateRoute & authenticated using user email and set LoadingState

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
+import LoadingSpinner from "../components/LoadingState/LoadingSpinner";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center pt-[27%]">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (user?.email) {
+    return children;
+  }
+
+  return <Navigate state={location?.pathname} to="/login"></Navigate>;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
+};
+
+export default PrivateRoute;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,33 +6,38 @@ import RegisterPage from "../pages/Registration/RegisterPage";
 import LoginPage from "../pages/Login/LoginPage";
 import AddFoodPage from "../pages/AddFoodPage/AddFoodPage";
 import AllFoodPage from "../pages/AllFoodPage/AllFoodPage";
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout></MainLayout>,
-    errorElement: <ErrorPage></ErrorPage>,
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <HomePage></HomePage>,
+        element: <HomePage />,
       },
       {
         path: "/add-food-item",
-        element: <AddFoodPage></AddFoodPage>,
+        element: (
+          <PrivateRoute>
+            <AddFoodPage />
+          </PrivateRoute>
+        ),
       },
       {
         path: "/all-food-items",
-        element: <AllFoodPage></AllFoodPage>,
+        element: <AllFoodPage />,
       },
     ],
   },
   {
     path: "/register",
-    element: <RegisterPage></RegisterPage>,
+    element: <RegisterPage />,
   },
   {
     path: "/login",
-    element: <LoginPage></LoginPage>,
+    element: <LoginPage />,
   },
 ]);
